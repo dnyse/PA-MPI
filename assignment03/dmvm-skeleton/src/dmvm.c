41502b07089a5660bb4d495916ef1fea8a9742b9
Dennys Huber <dennys.huber@proton.me>
2025-11-06 21:43:17 +0100

    continued working on assignment03

--- assignment03/dmvm-skeleton/src/dmvm.c
+++ assignment03/dmvm-skeleton/src/dmvm.c
@@ -11,27 +11,43 @@
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+#define PARALLEL
+#define CHECK
+
 double dmvm(double *restrict y, const double *restrict a,
             const double *restrict x, int N, int iter) {
   double ts, te;
+#ifdef PARALLEL
   int size, num, rest, rank, upperNeighbor, lowerNeighbor, Nlocal, cs, Ncurrent;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   num = N / size;
   rest = N % size;
+  Nlocal = (N / size) + ((N % size > rank) ? 1 : 0);
+
+  int x_start = rank * num + MIN(rest, rank);
+
+  double *x_local = (double *)malloc(Nlocal * sizeof(double));
+
+  for (int i = 0; i < Nlocal; i++) {
+    x_local[i] = x[x_start + i];
+  }
+
   upperNeighbor = (rank - 1) % size;
+  lowerNeighbor = (rank + 1) % size;
   if (upperNeighbor < 0)
     upperNeighbor = size - 1;
-  lowerNeighbor = (rank + 1) % size;
-  Nlocal = (N / size) + ((N % size > rank) ? 1 : 0);
-  cs = rank * (N / size) + MIN(rest, rank);
+
+  cs = x_start;
   Ncurrent = Nlocal;
 
+  ts = getTimeStamp();
   for (int rot = 0; rot < size; rot++) {
     for (int r = 0; r < Nlocal; r++) {
+      int r_local = x_start + r;
       for (int c = cs; c < cs + Ncurrent; c++) {
-        y[r] += a[r * N + c] * x[c - cs];
+        y[r_local] += a[r_local * N + c] * x_local[c - cs];
       }
     }
     cs += Ncurrent;
@@ -39,13 +55,13 @@ double dmvm(double *restrict y, const double *restrict a,
       cs = 0; // wrap around
     Ncurrent = (N / size) + ((N % size > (rank + rot + 1) % size) ? 1 : 0);
     if (rot != size - 1)
-      MPI_Sendrecv_replace(x, num + (rest ? 1 : 0), MPI_DOUBLE, upperNeighbor,
-                           0, lowerNeighbor, 0, MPI_COMM_WORLD,
-                           MPI_STATUS_IGNORE);
+      MPI_Sendrecv_replace(x_local, Nlocal, MPI_DOUBLE, upperNeighbor, 0,
+                           lowerNeighbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
-
-  ts = getTimeStamp();
+  te = getTimeStamp();
+#endif
 #ifdef SERIAL
+  ts = getTimeStamp();
   for (int j = 0; j < iter; j++) {
     for (int r = 0; r < N; r++) {
       for (int c = 0; c < N; c++) {
@@ -53,9 +69,10 @@ double dmvm(double *restrict y, const double *restrict a,
       }
     }
   }
+  te = getTimeStamp();
 #endif
 #ifdef CHECK
-  {
+  if (rank == 0) {
     double sum = 0.0;
 
     for (int i = 0; i < N; i++) {
@@ -65,6 +82,5 @@ double dmvm(double *restrict y, const double *restrict a,
     fprintf(stderr, "Sum: %f\n", sum);
   }
 #endif
-  te = getTimeStamp();
   return te - ts;
 }
